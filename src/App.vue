7080320759613c2c4e6192c90d4db6f882a467e2
Jason Javier <jasonjjavier@gmail.com>
2026-04-05 21:35:23 -0300

    added vitest

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white shadow-lg rounded-2xl p-8 text-center w-100">
+      
+      <h1 class="text-4xl font-semibold mb-4">
+        Vitest Demo
+      </h1>
+      <p>
+        {
+        "id": 1,
+        "name": "Classic Margherita Pizza",
+        "ingredients": [
+          "Pizza dough",
+          "Tomato sauce",
+          "Fresh mozzarella cheese",
+          "Fresh basil leaves",
+          "Olive oil",
+          "Salt and pepper to taste"
+        ],
+        "instructions": [
+          "Preheat the oven to 475°F (245°C).",
+          "Roll out the pizza dough and spread tomato sauce evenly.",
+          "Top with slices of fresh mozzarella and fresh basil leaves.",
+          "Drizzle with olive oil and season with salt and pepper.",
+          "Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.",
+          "Slice and serve hot."
+        ],
+        "prepTimeMinutes": 20,
+        "cookTimeMinutes": 15,
+        "servings": 4,
+        "difficulty": "Easy",
+        "cuisine": "Italian",
+        "caloriesPerServing": 300,
+        "tags": [
+          "Pizza",
+          "Italian"
+        ],
+        "userId": 45,
+        "image": "https://cdn.dummyjson.com/recipe-images/1.webp",
+        "rating": 4.6,
+        "reviewCount": 3,
+        "mealType": [
+          "Dinner"
+        ]
+      }
+      </p>
+      
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
